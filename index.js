0c11d3c930ef2c674ad9d7550da0db337ea715a1
Roberto Busó-García <robertobuso@gmail.com>
2018-07-03 13:48:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,66 +10,56 @@ function setCart(c) {
 }
 
 function addToCart(item) {
-var price = Math.floor (Math.random() * 100);
-var newObject = { itemName: item, itemPrice: price };
-cart.push(newObject);
-     return item + " has been added to your cart.";
+  var price = Math.floor (Math.random() * 100);
+  var newObject = { itemName: item, itemPrice: price };
+  cart.push(newObject);
+  return item + " has been added to your cart.";
 }
 
 function viewCart() {
   var cartLength = cart.length;
-  
   if (cartLength < 1) {
-        return "Your shopping cart is empty.";
-      } else if (cartLength === 1) {
-        return "In your cart, you have " + cart[0].itemName + " at $" + cart[0].itemPrice + ".";
-      } else {
-        var middleText = [];
-        var i = 0;
-        while (i != cartLength - 1) {
-         middleText[i] = " " + cart[i].itemName + " at $" + cart[i].itemPrice;
-          i++;
-        }
-        return "In your cart, you have" + middleText + ", and " + cart[cartLength-1].itemName + " at $" + cart[cartLength-1].itemPrice + ".";}
-}
-function total() {
-  var totalValue = 0;
-  var i = 0;
-  while (i <= cart.length) {
-      if (i < cart.length) {
-        totalValue = totalValue + cart[i].itemPrice;
-       i++;
-      } else {
-       return totalValue;
-  }
+    return "Your shopping cart is empty.";
+  } else if (cartLength === 1) {
+    return "In your cart, you have " + cart[0].itemName + " at $" + cart[0].itemPrice + ".";
+  } else {
+    var middleText = [];
+    var i = 0;
+    while (i != cartLength - 1) {
+      middleText[i] = " " + cart[i].itemName + " at $" + cart[i].itemPrice;
+      i++;
+    }
+    return "In your cart, you have" + middleText + ", and " + cart[cartLength-1].itemName + " at $" + cart[cartLength-1].itemPrice + ".";
   }
 }
 
-function removeFromCart(item) {
+function total() {
+  var totalValue = 0;
+  var i = 0;
+  while (i < cart.length) {
+    totalValue = totalValue + cart[i].itemPrice;
+    i++;
+  }
+  return totalValue;
+}
+
+function removeFromCart(unnecessaryItem) {
   for (var i = 0; i < cart.length; i++) {
-    if (cart[i].itemName === item) {
+    if (cart[i].itemName === unnecessaryItem) {
       cart.splice(i, 1);
       return cart;
     }
-}
+  }
   return "That item is not in your cart.";
 }
 
 function placeOrder(cardNumber) {
-  if (cardNumber == null) {
-    return "Sorry, we don't have a credit card on file for you.";
+  if (cardNumber) {
+    var totalValue = total();
+    cart = [];
+    return "Your total cost is $" + totalValue + ", which will be charged to the card " + cardNumber + ".";
   } else {
-    var totalValue = 0;
-    var i = 0;
-    while (i < cart.length) {
-       debugger;
-        totalValue = totalValue + cart[i].itemPrice;
-       i++;
+    return "Sorry, we don't have a credit card on file for you.";
   }
-  cart = [];
-  
-  return "Your total cost is $" + totalValue + ", which will be charged to the card " + cardNumber + ".";
-}
-
 }
 
